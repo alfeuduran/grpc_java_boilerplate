e90ef14d3e797729fbf6103730f6a0b41df62c60
alfeuduran <36578943+alfeuduran@users.noreply.github.com>
2021-03-03 06:30:52 -0300

    boilerplate commit

---
+++ src/main/java/com/github/alfeuduran/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,28 @@
+package com.github.alfeuduran.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello GRPC!");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        //Gracefuly shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+            System.out.println("Received SHutdown Request");
+            server.shutdown();
+            System.out.println("Successfully sttoped the server");
+        }));
+
+        server.awaitTermination();
+
+    }
+}
