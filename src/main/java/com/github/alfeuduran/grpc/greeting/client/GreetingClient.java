c0c0483cd440d3a9c6688afdedf183691e44b3cf
alfeuduran <alfeuduran@gmail.com>
2021-03-06 18:52:38 -0300

    Add Client Streaming code test

--- src/main/java/com/github/alfeuduran/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/alfeuduran/grpc/greeting/client/GreetingClient.java
@@ -4,10 +4,14 @@ import com.proto.dummy.DummyServiceGrpc;
 import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class GreetingClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         GreetingClient main = new GreetingClient();
         main.run();
@@ -15,18 +19,17 @@ public class GreetingClient {
     }
 
 
-    public void run (){
+    public void run () throws InterruptedException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
         //doUnaryCall(channel);
         //doServerStreamingCall(channel);
+        doClientStreamingCall(channel);
 
         System.out.println("Shutting down channel");
         channel.shutdown();
-
-
     }
 
     private void doUnaryCall(ManagedChannel channel){
@@ -76,5 +79,72 @@ public class GreetingClient {
 
     }
 
+    private void doClientStreamingCall(ManagedChannel channel) throws InterruptedException {
+        //create a client (stub)
+        //GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        //create an Async cliet
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<LongGreetRequest> requestObserver =  asyncClient.longGreet(new StreamObserver<LongGreetResponse>() {
+            @Override
+            public void onNext(LongGreetResponse value) {
+                // quando temos uma resposta do server
+                System.out.println("Received response from the server");
+                System.out.println(value.getResult());
+                //Ira ser chamado apenas uma vez
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // quando temos um erro do server
+
+            }
+
+            @Override
+            public void onCompleted() {
+                // O server terminou de enviar os dados.
+                System.out.println("Server has completed sending us something");
+                // Ira ser chamado depois do onNext()
+                latch.countDown();
+            }
+        });
+
+
+        // apenas um lembrete que como isso ira ser async temos que fazer a implementacao de um latch.
+
+
+        // Streaming Message 1
+        System.out.println("Sending Message 1");
+
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+        .setGreeting(Greeting.newBuilder()
+        .setFirstName("Alfeu")
+        .build())
+                .build());
+
+        // Streaming Message 2
+        System.out.println("Sending Message 2");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Duran")
+                        .build())
+                .build());
+
+        // Streaming Message 3
+        System.out.println("Sending Message 3");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Santos")
+                        .build())
+                .build());
+
+        //Falamos para o server que o client acabou de enviar dados
+        requestObserver.onCompleted();
+
+        latch.await(3L, TimeUnit.SECONDS);
+    }
 
 }
