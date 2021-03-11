a4117a2c3568f89d0fd0392a0c6d02c92b6a4fce
alfeuduran <alfeuduran@gmail.com>
2021-03-11 07:08:31 -0300

    implement blog api

--- src/main/java/com/github/alfeuduran/grpc/greeting/blog/client/BlogClient.java
+++ src/main/java/com/github/alfeuduran/grpc/greeting/blog/client/BlogClient.java
@@ -1,6 +1,12 @@
 package com.github.alfeuduran.grpc.greeting.blog.client;
 
 import com.github.alfeuduran.grpc.greeting.client.GreetingClient;
+import com.proto.blog.Blog;
+import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class BlogClient {
 
@@ -14,6 +20,33 @@ public class BlogClient {
 
 
     public void run () throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+
+        BlogServiceGrpc.BlogServiceBlockingStub blogClient = BlogServiceGrpc.newBlockingStub(channel);
+
+        Blog blog = Blog.newBuilder()
+                .setAuthorId("Alfeu")
+                .setTitle("New Blog")
+                .setContent("Hello world this is my first blog!")
+                .build();
+
+        CreateBlogResponse createResponse =  blogClient.createBlog(
+                CreateBlogRequest.newBuilder()
+                        .setBlog(blog)
+                        .build()
+        );
+
+        System.out.println("Received create blog response from the client");
+        System.out.println(createResponse.toString());
+
+
+
+
+
+
 
     }
 
--- src/main/java/com/github/alfeuduran/grpc/greeting/blog/server/BlogServiceImpl.java
+++ src/main/java/com/github/alfeuduran/grpc/greeting/blog/server/BlogServiceImpl.java
@@ -1,8 +1,55 @@
 package com.github.alfeuduran.grpc.greeting.blog.server;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.proto.blog.Blog;
 import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.stub.StreamObserver;
+import org.bson.Document;
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
+
+    private MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+    private MongoDatabase database = mongoClient.getDatabase("mydb");
+    private MongoCollection<Document> collection = database.getCollection("blog");
+
+    @Override
+    public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
+
+
+        System.out.println("Received Create Blog Request");
+
+        Blog blog = request.getBlog();
+
+        Document doc = new Document("author_id", blog.getAuthorId())
+                .append("title",blog.getTitle())
+                .append("content",blog.getContent());
+
+
+        System.out.println("Inserting Blog..");
+        collection.insertOne(doc);
+        System.out.println("Blog Inserted");
+
+        String id = doc.getObjectId("_id").toString(); // id que é criado pelo Mongo
+        System.out.println("Return String " + id);
+
+
+        CreateBlogResponse response = CreateBlogResponse.newBuilder()
+                .setBlog(blog.toBuilder()
+                .setId(id))
+                .build();
+
+
+        responseObserver.onNext(response);
+
+        responseObserver.onCompleted();
+
+    }
+
 
 }
 
